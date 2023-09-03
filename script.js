7148cc86639a214e9e6909f8583158d1b574b2f8
MoonKnight <aqdas062@gmail.com>
2023-09-03 13:49:47 +0530

    Updated Code

--- script.js
+++ script.js
@@ -1,9 +1,3 @@
-// const contact_btn = document.getElementById("contact-btn");
-
-// contact_btn.onclick = () => {
-//   alert("Yes")
-// };
-
 const mode_btn = document.getElementById("mode-btn");
 const circle_btn = document.getElementById("circle-toggle");
 
@@ -38,13 +32,61 @@ for (let i = 0 ; i < letters_about.length; i++) {
   });
   letters_about[i].addEventListener("mouseover",() => {
     letters_about[i].classList.add("animated");
-  })
-}
+  });
+};
 
 function changeColor(icon,condition) {
   if (condition) {
     icon.style = "color: var(--nav-bar-icon-hover-color);"
   } else {
     icon.style = "color: var(--nav-bar-icon-color);"
-  }
+    }
 }
+
+const home_icon = document.getElementById("home-icon");
+const about_icon = document.getElementById("about-icon");
+const skills_icon = document.getElementById("skills-icon");
+const projects_icon = document.getElementById("projects-icon");
+const contact_icon = document.getElementById("contact-icon");
+const icons = [home_icon,about_icon,skills_icon,projects_icon,contact_icon];
+
+for (let i = 0; i < icons.length; i++) {
+  icons[i].addEventListener("mouseenter",() => {
+    switch(i) {
+      case 0: 
+      icons[i].innerHTML = '<a href="#home">HOME</a>';
+      break;
+      case 1: 
+      icons[i].innerHTML = '<a href="#home">ABOUT</a>';
+      break;
+      case 2: 
+      icons[i].innerHTML = '<a href="#home">SKILLS</a>';
+      break;
+      case 3: 
+      icons[i].innerHTML = '<a href="#home">PROJECTS</a>';
+      break;
+      case 4: 
+      icons[i].innerHTML = '<a href="#home">CONTACT</a>';
+      break;
+    }
+  });
+  icons[i].addEventListener("mouseleave",() => {
+    switch(i) {
+      case 0:
+        icons[i].innerHTML = '<a href="#home"><i class="fa-solid fa-house fa-lg" style="color: var(--nav-bar-icon-color)" onmouseover="changeColor(this,true)" onmouseleave="changeColor(this,false)"></i></a>'; 
+      break;
+      case 1: 
+      icons[i].innerHTML = '<a href="#home"><i class="fa-solid fa-user fa-lg" style="color: var(--nav-bar-icon-color)" onmouseover="changeColor(this,true)" onmouseleave="changeColor(this,false)"></i></a>'; 
+      break;
+      case 2: 
+      icons[i].innerHTML = '<a href="#home"><i class="fa-solid fa-gear fa-lg" style="color: var(--nav-bar-icon-color)" onmouseover="changeColor(this,true)" onmouseleave="changeColor(this,false)"></i></a>'; 
+      break;
+      case 3: 
+      icons[i].innerHTML = '<a href="#home"><i class="fa-solid fa-file fa-lg" style="color: var(--nav-bar-icon-color)" onmouseover="changeColor(this,true)" onmouseleave="changeColor(this,false)"></i></a>'; 
+      break;
+      case 4: 
+      icons[i].innerHTML = '<a href="#home"><i class="fa-solid fa-envelope fa-lg" style="color: var(--nav-bar-icon-color)" onmouseover="changeColor(this,true)" onmouseleave="changeColor(this,false)"></i></a>'; 
+      break;
+    }
+  });
+};
